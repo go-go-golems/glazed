8beead21769f2d474ec33093b9d61c4197c3797a
Manuel Odendahl <wesen@ruinwesen.com>
2022-11-24 22:11:50 -0500

    :sparkles: Add support for yaml subcommand and single object yaml input

--- cmd/glaze/main.go
+++ cmd/glaze/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"glazed/pkg/cli"
 	"glazed/pkg/formatters"
 	"glazed/pkg/middlewares"
 	"glazed/pkg/types"
+	"gopkg.in/yaml.v3"
 	"os"
 )
 
@@ -41,69 +43,71 @@ func (gp *GlazeProcessor) processInputObject(obj map[string]interface{}) error {
 	return nil
 }
 
-var jsonCmd = &cobra.Command{
-	Use:   "json",
-	Short: "Format JSON data",
+var yamlCmd = &cobra.Command{
+	Use:   "yaml",
+	Short: "Format YAML data",
 	Run: func(cmd *cobra.Command, args []string) {
-
 		if len(args) == 0 {
 			fmt.Println("No input file specified")
 			os.Exit(1)
 		}
 
-		outputSettings, err := cli.ParseOutputFlags(cmd)
+		gp, of, err := setupProcessor(cmd)
 		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Error parsing output flags: %v\n", err)
+			_, _ = fmt.Fprintf(os.Stderr, "Could not create Glaze processor: %v\n", err)
 			os.Exit(1)
 		}
 
-		templateSettings, err := cli.ParseTemplateFlags(cmd)
+		for _, arg := range args {
+			inputBytes, err := os.ReadFile(arg)
+
+			data := make(map[interface{}]interface{})
+			data2 := make(map[string]interface{})
+
+			err = yaml.Unmarshal(inputBytes, data)
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "Error decoding file %s as object: %s\n", arg, err)
+				os.Exit(1)
+			}
+			for k, v := range data {
+				data2[fmt.Sprintf("%v", k)] = v
+			}
+			err = gp.processInputObject(data2)
+			if err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "Error processing file %s as object: %s\n", arg, err)
+				os.Exit(1)
+			}
+		}
+
+		s, err := of.Output()
 		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Error parsing template flags: %v\n", err)
+			_, _ = fmt.Fprintf(os.Stderr, "Error rendering output: %s\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(s)
+	},
+}
+
+var jsonCmd = &cobra.Command{
+	Use:   "json",
+	Short: "Format JSON data",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("No input file specified")
 			os.Exit(1)
 		}
 
-		fieldsFilterSettings, err := cli.ParseFieldsFilterFlags(cmd)
+		gp, of, err := setupProcessor(cmd)
 		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Error parsing fields filter flags: %v\n", err)
+			_, _ = fmt.Fprintf(os.Stderr, "Could not create glaze  procersors: %v\n", err)
 			os.Exit(1)
 		}
-
-		of, err := outputSettings.CreateOutputFormatter()
-		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Error creating output formatter: %v\n", err)
-			os.Exit(1)
-		}
-
-		err = templateSettings.AddMiddlewares(of)
-		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "Error adding template middlewares: %v\n", err)
-			os.Exit(1)
-		}
-
-		if (outputSettings.Output == "json" || outputSettings.Output == "yaml") && outputSettings.FlattenObjects {
-			mw := middlewares.NewFlattenObjectMiddleware()
-			of.AddTableMiddleware(mw)
-		}
-		fieldsFilterSettings.AddMiddlewares(of)
 
 		inputIsArray, err := cmd.Flags().GetBool("input-is-array")
 		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "Error parsing input-is-array flag: %v\n", err)
 			os.Exit(1)
 		}
-
-		var middlewares_ []middlewares.ObjectMiddleware
-		if templateSettings.UseRowTemplates {
-			ogtm, err := middlewares.NewObjectGoTemplateMiddleware(templateSettings.Templates)
-			if err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "Error creating object template middleware: %v\n", err)
-				os.Exit(1)
-			}
-			middlewares_ = append(middlewares_, ogtm)
-		}
-
-		gp := NewGlazeProcessor(of, middlewares_)
 
 		for _, arg := range args {
 			f, err := os.Open(arg)
@@ -154,6 +158,51 @@ var jsonCmd = &cobra.Command{
 	},
 }
 
+func setupProcessor(cmd *cobra.Command) (*GlazeProcessor, formatters.OutputFormatter, error) {
+	outputSettings, err := cli.ParseOutputFlags(cmd)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "Error parsing output flags")
+	}
+
+	templateSettings, err := cli.ParseTemplateFlags(cmd)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "Error parsing template flags")
+	}
+
+	fieldsFilterSettings, err := cli.ParseFieldsFilterFlags(cmd)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "Error parsing fields filter flags")
+	}
+
+	of, err := outputSettings.CreateOutputFormatter()
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "Error creating output formatter")
+	}
+
+	err = templateSettings.AddMiddlewares(of)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "Error adding template middlewares")
+	}
+
+	if (outputSettings.Output == "json" || outputSettings.Output == "yaml") && outputSettings.FlattenObjects {
+		mw := middlewares.NewFlattenObjectMiddleware()
+		of.AddTableMiddleware(mw)
+	}
+	fieldsFilterSettings.AddMiddlewares(of)
+
+	var middlewares_ []middlewares.ObjectMiddleware
+	if templateSettings.UseRowTemplates {
+		ogtm, err := middlewares.NewObjectGoTemplateMiddleware(templateSettings.Templates)
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, "Could not process template argument")
+		}
+		middlewares_ = append(middlewares_, ogtm)
+	}
+
+	gp := NewGlazeProcessor(of, middlewares_)
+	return gp, of, nil
+}
+
 func main() {
 	_ = rootCmd.Execute()
 }
@@ -169,5 +218,11 @@ func init() {
 	// json input options
 	jsonCmd.Flags().Bool("input-is-array", false, "Input is an array of objects")
 
+	yamlCmd.Flags().SortFlags = false
+	cli.AddOutputFlags(yamlCmd)
+	cli.AddTemplateFlags(yamlCmd)
+	cli.AddFieldsFilterFlags(yamlCmd)
+
 	rootCmd.AddCommand(jsonCmd)
+	rootCmd.AddCommand(yamlCmd)
 }
