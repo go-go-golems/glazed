694c225868feac5e4bc54a64b7eeac729f15f5a7
Manuel Odendahl <wesen@ruinwesen.com>
2022-12-28 14:55:26 -0500

    :sparkles: Add unit tests for YAML parsing

--- pkg/middlewares/table_rename_test.go
+++ pkg/middlewares/table_rename_test.go
@@ -4,7 +4,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wesen/glazed/pkg/types"
+	"gopkg.in/yaml.v3"
 	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -162,4 +164,115 @@ func createTestTable() *types.Table {
 	})
 
 	return &table
+}
+
+func TestBothFieldAndRegexpRenames(t *testing.T) {
+	renameTable := map[types.FieldName]types.FieldName{
+		"foo": "bar",
+		"baz": "qux",
+	}
+	rrs := RegexpReplacements{}
+	rrs = append(rrs, &RegexpReplacement{
+		regexp.MustCompile("f.."), "bar2",
+	})
+
+	mw := NewRenameColumnMiddleware(renameTable, rrs)
+	table := createTestTable()
+
+	newTable, ret := mw.Process(table)
+	require.Nil(t, ret)
+
+	row := newTable.Rows[0].(*types.SimpleRow)
+	rowMap := row.GetValues()
+
+	assert.Nil(t, rowMap["foo"])
+	assert.Nil(t, rowMap["baz"])
+	assert.Nil(t, rowMap["foobar"])
+	assert.Equal(t, 1, rowMap["bar"])
+	assert.Equal(t, 2, rowMap["qux"])
+	assert.Equal(t, 3, rowMap["bar2bar"])
+}
+
+func TestParseFromYAML(t *testing.T) {
+	yamlString := `
+renames:
+  foo: bar
+  baz: qux
+`
+	decoder := yaml.NewDecoder(strings.NewReader(yamlString))
+	mw, err := NewRenameColumnMiddlewareFromYAML(decoder)
+	require.Nil(t, err)
+
+	table := createTestTable()
+
+	newTable, ret := mw.Process(table)
+	require.Nil(t, ret)
+
+	row := newTable.Rows[0].(*types.SimpleRow)
+	rowMap := row.GetValues()
+
+	assert.Nil(t, rowMap["foo"])
+	assert.Nil(t, rowMap["baz"])
+	assert.Equal(t, 1, rowMap["bar"])
+	assert.Equal(t, 2, rowMap["qux"])
+	assert.Equal(t, 3, rowMap["foobar"])
+}
+
+func TestParseRegexpFromYAML(t *testing.T) {
+	yamlString := `
+regexpRenames:
+  "^foo$": bar2
+  f..: bar
+  "^baz$": qux
+`
+
+	decoder := yaml.NewDecoder(strings.NewReader(yamlString))
+	mw, err := NewRenameColumnMiddlewareFromYAML(decoder)
+	require.Nil(t, err)
+
+	table := createTestTable()
+
+	newTable, ret := mw.Process(table)
+	require.Nil(t, ret)
+
+	row := newTable.Rows[0].(*types.SimpleRow)
+	rowMap := row.GetValues()
+
+	assert.Nil(t, rowMap["foo"])
+	assert.Nil(t, rowMap["baz"])
+	assert.Nil(t, rowMap["foobar"])
+	assert.Equal(t, 1, rowMap["bar2"])
+	assert.Equal(t, 2, rowMap["qux"])
+	assert.Equal(t, 3, rowMap["barbar"])
+}
+
+func TestParseBothFromYAML(t *testing.T) {
+	yamlString := `
+renames:
+  foo: bar
+  baz: qux
+regexpRenames:
+  "^foo$": bar2
+  f..: bar
+  "^baz$": qux2
+`
+
+	decoder := yaml.NewDecoder(strings.NewReader(yamlString))
+	mw, err := NewRenameColumnMiddlewareFromYAML(decoder)
+	require.Nil(t, err)
+
+	table := createTestTable()
+
+	newTable, ret := mw.Process(table)
+	require.Nil(t, ret)
+
+	row := newTable.Rows[0].(*types.SimpleRow)
+	rowMap := row.GetValues()
+
+	assert.Nil(t, rowMap["foo"])
+	assert.Nil(t, rowMap["baz"])
+	assert.Nil(t, rowMap["foobar"])
+	assert.Equal(t, 1, rowMap["bar"])
+	assert.Equal(t, 2, rowMap["qux"])
+	assert.Equal(t, 3, rowMap["barbar"])
 }
