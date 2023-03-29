9a8f896c2fb3173c4cd36e9e77a3566dae45f519
Manuel Odendahl <wesen@ruinwesen.com>
2023-03-29 11:08:14 -0400

    :sparkles: Add strip tags

--- cmd/glaze/cmds/html/parse_test.go
+++ cmd/glaze/cmds/html/parse_test.go
@@ -94,3 +94,104 @@ func TestTwoHeadersParse(t *testing.T) {
 	assert.Equal(t, "Subheader", gp.Objects[1]["Title"])
 	assert.Equal(t, "h2", gp.Objects[1]["Tag"])
 }
+
+func TestTwoHeadersBody(t *testing.T) {
+	gp := NewTestProcessor()
+	// go:filetype html
+	html_ := `
+<html>
+	<head>
+		<title>Foobar</title>
+	</head>
+	<body>
+		<h1>Header</h1>
+		<h2>Subheader</h2>
+	</body>
+</html>
+`
+
+	doc, err := html.Parse(strings.NewReader(html_))
+	require.NoError(t, err)
+
+	hsp := NewHTMLHeadingSplitParser(gp, []string{})
+
+	n, err := hsp.ProcessNode(doc)
+	require.NoError(t, err)
+	assert.Nil(t, n)
+
+	assert.Equal(t, 2, len(gp.Objects))
+	assert.Equal(t, "Header", gp.Objects[0]["Title"])
+	assert.Equal(t, "h1", gp.Objects[0]["Tag"])
+	assert.Equal(t, "Subheader", gp.Objects[1]["Title"])
+	assert.Equal(t, "h2", gp.Objects[1]["Tag"])
+}
+
+func TestTwoHeadersSomeTextBody(t *testing.T) {
+	gp := NewTestProcessor()
+
+	html_ := `
+<html>
+	<head>
+		<title>Foobar</title>
+	</head>
+	<body>
+		<h1>Header</h1>
+		<p>Some text</p>
+		<h2>Subheader</h2>
+		<p>Some text</p>
+	</body>
+</html>
+`
+
+	doc, err := html.Parse(strings.NewReader(html_))
+	require.NoError(t, err)
+
+	hsp := NewHTMLHeadingSplitParser(gp, []string{})
+
+	n, err := hsp.ProcessNode(doc)
+	require.NoError(t, err)
+	assert.Nil(t, n)
+
+	assert.Equal(t, 2, len(gp.Objects))
+	assert.Equal(t, "Header", gp.Objects[0]["Title"])
+	assert.Equal(t, "h1", gp.Objects[0]["Tag"])
+	assert.Equal(t, "<p>Some text</p>", gp.Objects[0]["Body"])
+
+	assert.Equal(t, "Subheader", gp.Objects[1]["Title"])
+	assert.Equal(t, "h2", gp.Objects[1]["Tag"])
+	assert.Equal(t, "<p>Some text</p>", gp.Objects[1]["Body"])
+}
+
+func TestStripTags(t *testing.T) {
+	gp := NewTestProcessor()
+	html_ := `
+<html>
+	<head>
+		<title>Foobar</title>
+	</head>
+	<body>
+		<h1>Header</h1>
+		<p>Some text<b>Foobar</b> <span><strong>Test</strong></span></p>
+		<h2>Subheader <strong>Foobar</strong><span>Test</span></h2>
+		<p>Some text</p>
+	</body>
+</html>
+`
+	doc, err := html.Parse(strings.NewReader(html_))
+	require.NoError(t, err)
+
+	hsp := NewHTMLHeadingSplitParser(gp, []string{"span", "p"})
+
+	n, err := hsp.ProcessNode(doc)
+	require.NoError(t, err)
+	assert.Nil(t, n)
+
+	assert.Equal(t, 2, len(gp.Objects))
+	assert.Equal(t, "Header", gp.Objects[0]["Title"])
+	assert.Equal(t, "h1", gp.Objects[0]["Tag"])
+	assert.Equal(t, "Some text<b>Foobar</b> <strong>Test</strong>", gp.Objects[0]["Body"])
+
+	assert.Equal(t, "Subheader <strong>Foobar</strong>Test", gp.Objects[1]["Title"])
+	assert.Equal(t, "h2", gp.Objects[1]["Tag"])
+	assert.Equal(t, "Some text", gp.Objects[1]["Body"])
+}
