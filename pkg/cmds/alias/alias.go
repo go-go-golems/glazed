c507b5e4d5e7be3214988dd1f2d7ce1cdf741add
Manuel Odendahl <wesen@ruinwesen.com>
2023-04-15 18:53:24 -0400

    :tractor: Refactor cmds loading to separate packages, add alias options

--- pkg/cmds/alias/alias.go
+++ pkg/cmds/alias/alias.go
@@ -1,12 +1,17 @@
-package cmds
+package alias
 
 import (
 	"context"
 	"fmt"
+	"github.com/go-go-golems/glazed/pkg/cmds"
 	"github.com/go-go-golems/glazed/pkg/cmds/layers"
 	"github.com/go-go-golems/glazed/pkg/cmds/parameters"
 	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+	"io"
 )
+
+type Option func(*CommandAlias)
 
 // CommandAlias defines a struct that should be able to define generic aliases
 // for any kind of command line applications, by providing overrides for certain
@@ -17,9 +22,91 @@ type CommandAlias struct {
 	Flags     map[string]string `yaml:"flags,omitempty"`
 	Arguments []string          `yaml:"arguments,omitempty"`
 
-	AliasedCommand Command  `yaml:",omitempty"`
-	Parents        []string `yaml:",omitempty"`
-	Source         string   `yaml:",omitempty"`
+	AliasedCommand cmds.Command `yaml:",omitempty"`
+	Parents        []string     `yaml:",omitempty"`
+	Source         string       `yaml:",omitempty"`
+}
+
+func WithName(name string) Option {
+	return func(a *CommandAlias) {
+		a.Name = name
+	}
+}
+
+func WithAliasFor(aliasFor string) Option {
+	return func(a *CommandAlias) {
+		a.AliasFor = aliasFor
+	}
+}
+
+func WithFlags(flags map[string]string) Option {
+	return func(a *CommandAlias) {
+		for k, v := range flags {
+			a.Flags[k] = v
+		}
+	}
+}
+
+func WithArguments(arguments []string) Option {
+	return func(a *CommandAlias) {
+		a.Arguments = arguments
+	}
+}
+
+func WithParents(parents ...string) Option {
+	return func(a *CommandAlias) {
+		a.Parents = parents
+	}
+}
+
+func WithStripParentsPrefix(prefixes []string) Option {
+	return func(a *CommandAlias) {
+		toRemove := 0
+		for i, p := range a.Parents {
+			if i < len(prefixes) && p == prefixes[i] {
+				toRemove = i + 1
+			}
+		}
+		a.Parents = a.Parents[toRemove:]
+	}
+}
+
+func WithSource(source string) Option {
+	return func(a *CommandAlias) {
+		a.Source = source
+	}
+}
+
+func WithPrependSource(source string) Option {
+	return func(a *CommandAlias) {
+		a.Source = source + a.Source
+	}
+}
+
+func NewCommandAlias(options ...Option) *CommandAlias {
+	a := &CommandAlias{
+		Flags: make(map[string]string),
+	}
+	for _, option := range options {
+		option(a)
+	}
+	return a
+}
+
+func NewCommandAliasFromYAML(s io.Reader, options ...Option) (*CommandAlias, error) {
+	a := NewCommandAlias()
+	if err := yaml.NewDecoder(s).Decode(a); err != nil {
+		return nil, err
+	}
+	if !a.IsValid() {
+		return nil, errors.New("Invalid command alias")
+	}
+
+	for _, option := range options {
+		option(a)
+	}
+
+	return a, nil
 }
 
 func (a *CommandAlias) String() string {
@@ -31,12 +118,12 @@ func (a *CommandAlias) Run(
 	ctx context.Context,
 	parsedLayers map[string]*layers.ParsedParameterLayer,
 	ps map[string]interface{},
-	gp Processor,
+	gp cmds.Processor,
 ) error {
 	if a.AliasedCommand == nil {
 		return errors.New("no aliased command")
 	}
-	glazeCommand, ok := a.AliasedCommand.(GlazeCommand)
+	glazeCommand, ok := a.AliasedCommand.(cmds.GlazeCommand)
 	if !ok {
 		return errors.New("aliased command is not a GlazeCommand")
 	}
@@ -52,9 +139,9 @@ func (a *CommandAlias) IsValid() bool {
 // making copies of its flags and arguments.
 // This is necessary because they get mutated at runtime with various defaults,
 // depending on where they come from.
-func (a *CommandAlias) Description() *CommandDescription {
+func (a *CommandAlias) Description() *cmds.CommandDescription {
 	s := a.AliasedCommand.Description()
-	ret := &CommandDescription{
+	ret := &cmds.CommandDescription{
 		Name:      a.Name,
 		Short:     s.Short,
 		Long:      s.Long,
