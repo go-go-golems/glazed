60218ff708605b09ce1ab76ffdd439ed421a044a
Manuel Odendahl <wesen@ruinwesen.com>
2023-08-03 23:12:54 -0400

    :sparkles: Add lambda row middleware that filters through a function

---
+++ pkg/middlewares/row/lambda.go
@@ -0,0 +1,22 @@
+package row
+
+import (
+	"context"
+	"github.com/go-go-golems/glazed/pkg/types"
+)
+
+type LambdaMiddleware struct {
+	Function func(ctx context.Context, row types.Row) ([]types.Row, error)
+}
+
+func (l *LambdaMiddleware) Close(ctx context.Context) error {
+	return nil
+}
+
+func NewLambdaMiddleware(function func(ctx context.Context, row types.Row) ([]types.Row, error)) *LambdaMiddleware {
+	return &LambdaMiddleware{Function: function}
+}
+
+func (l *LambdaMiddleware) Process(ctx context.Context, row types.Row) ([]types.Row, error) {
+	return l.Function(ctx, row)
+}
