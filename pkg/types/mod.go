0a96a380409738e986b3911cb96d6c90b077aae4
Manuel Odendahl <wesen@ruinwesen.com>
2023-10-11 23:57:46 -0400

    :bulb: Add documentation for Row functions

--- pkg/types/mod.go
+++ pkg/types/mod.go
@@ -13,12 +13,16 @@ type GenericCellValue = interface{}
 type Row = *orderedmap.OrderedMap[FieldName, GenericCellValue]
 type MapRowPair = orderedmap.Pair[FieldName, GenericCellValue]
 
+// NewRow creates a new Row instance with the given initial data pairs.
+// The order of the pairs is preserved.
 func NewRow(initialData ...MapRowPair) Row {
 	return orderedmap.New[FieldName, GenericCellValue](
 		orderedmap.WithInitialData(initialData...),
 	)
 }
 
+// NewRowFromMap creates a new Row instance populated with the contents of the provided map.
+// The fields in the resulting Row are sorted alphabetically.
 func NewRowFromMap(hash map[FieldName]GenericCellValue) Row {
 	ret := NewRow()
 
@@ -35,6 +39,7 @@ func NewRowFromMap(hash map[FieldName]GenericCellValue) Row {
 	return ret
 }
 
+// NewRowFromRow creates a new Row instance with a deep copy of the contents from the provided row.
 func NewRowFromRow(row Row) Row {
 	ret := NewRow()
 
@@ -44,6 +49,8 @@ func NewRowFromRow(row Row) Row {
 	return ret
 }
 
+// NewRowFromMapWithColumns creates a new Row instance with values from the provided map,
+// but only for the columns specified. If a column doesn't exist in the map, it is skipped.
 func NewRowFromMapWithColumns(hash map[FieldName]GenericCellValue, columns []FieldName) Row {
 	ret := NewRow()
 	for _, column := range columns {
@@ -56,6 +63,9 @@ func NewRowFromMapWithColumns(hash map[FieldName]GenericCellValue, columns []Fie
 	return ret
 }
 
+// NewRowFromStruct creates a new Row instance populated with the field values from the provided struct.
+// The field names of the struct become the keys in the Row. If lowerCaseKeys is true,
+// the field names are converted to lowercase.
 func NewRowFromStruct(i interface{}, lowerCaseKeys bool) Row {
 	row := NewRow()
 	SetFromStruct(row, i, lowerCaseKeys)
@@ -63,6 +73,9 @@ func NewRowFromStruct(i interface{}, lowerCaseKeys bool) Row {
 	return row
 }
 
+// SetFromStruct populates the provided row with values from the given struct.
+// The field names of the struct become the keys in the Row. If lowerCaseKeys is true,
+// the field names are converted to lowercase. Returns the populated row.
 func SetFromStruct(row Row, i interface{}, lowerCaseKeys bool) Row {
 	val := reflect.ValueOf(i)
 	if val.Kind() == reflect.Ptr {
@@ -83,10 +96,14 @@ func SetFromStruct(row Row, i interface{}, lowerCaseKeys bool) Row {
 	return row
 }
 
+// MRP creates a new MapRowPair with the given key and value.
+// This is useful when creating a Row with NewRow.
 func MRP(key FieldName, value GenericCellValue) MapRowPair {
 	return orderedmap.Pair[FieldName, GenericCellValue]{Key: key, Value: value}
 }
 
+// RowToMap converts a Row to a standard Go map with string keys.
+// The keys lose their original order.
 func RowToMap(om Row) map[string]interface{} {
 	ret := make(map[string]interface{})
 	for pair := om.Oldest(); pair != nil; pair = pair.Next() {
@@ -95,6 +112,7 @@ func RowToMap(om Row) map[string]interface{} {
 	return ret
 }
 
+// GetFields retrieves a list of field names (keys) from the provided Row, in their original order.
 func GetFields(om Row) []FieldName {
 	ret := []FieldName{}
 	for pair := om.Oldest(); pair != nil; pair = pair.Next() {
