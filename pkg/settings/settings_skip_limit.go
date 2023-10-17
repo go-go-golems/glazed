57078dcaaae7ffa145f492719b7a88f21ca24d94
Manuel Odendahl <wesen@ruinwesen.com>
2023-10-16 20:44:59 -0400

    :sparkles: Add skip-limit layer to glazed

---
+++ pkg/settings/settings_skip_limit.go
@@ -0,0 +1,41 @@
+package settings
+
+import (
+	_ "embed"
+	"github.com/go-go-golems/glazed/pkg/cmds/layers"
+	"github.com/go-go-golems/glazed/pkg/cmds/parameters"
+	"github.com/pkg/errors"
+)
+
+//go:embed "flags/skip-limit.yaml"
+var skipLimitFlagsYaml []byte
+
+type SkipLimitSettings struct {
+	Skip  int `glazed.parameter:"skip"`
+	Limit int `glazed.parameter:"limit"`
+}
+
+func NewSkipLimitSettingsFromParameters(ps map[string]interface{}) (*SkipLimitSettings, error) {
+	s := &SkipLimitSettings{}
+	err := parameters.InitializeStructFromParameters(s, ps)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to initialize skipLimit settings from parameters")
+	}
+
+	return s, nil
+}
+
+type SkipLimitParameterLayer struct {
+	*layers.ParameterLayerImpl `yaml:",inline"`
+}
+
+func NewSkipLimitParameterLayer(options ...layers.ParameterLayerOptions) (*SkipLimitParameterLayer, error) {
+	ret := &SkipLimitParameterLayer{}
+	layer, err := layers.NewParameterLayerFromYAML(skipLimitFlagsYaml, options...)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create skipLimit parameter layer")
+	}
+	ret.ParameterLayerImpl = layer
+
+	return ret, nil
+}
