3f8163938623701f825025a6025a742b71c1985a
Manuel Odendahl <wesen@ruinwesen.com>
2023-10-28 13:56:55 -0400

    :art: Cleanup LLM code

--- pkg/helpers/capture-output.go
+++ pkg/helpers/capture-output.go
@@ -7,53 +7,37 @@ import (
 )
 
 // CaptureOutput executes a provided function and captures any data
-// the function writes to standard output.
+// the function writes to standard output. It returns the captured output
+// as a string and any error that occurred during execution or capturing.
+// If multiple errors occur, only the last one will be returned.
 func CaptureOutput(fn func() error) (string, error) {
 	var buf bytes.Buffer
-
-	// Save the original stdout to restore it later
 	originalStdout := os.Stdout
-
-	// Use a temporary pipe to capture output
 	r, w, err := os.Pipe()
 	if err != nil {
 		return "", err
 	}
 
-	// This is where you'll capture the output
 	os.Stdout = w
-
-	// Capture stdout in a separate goroutine to prevent deadlocks
 	captureDone := make(chan struct{})
 	go func() {
-		// Copy the data written to the PipeReader to the buffer
 		_, errCopy := io.Copy(&buf, r)
 		if errCopy != nil {
-			// This error will be handled later
 			err = errCopy
 		}
-
-		// Signal that capturing is complete
 		close(captureDone)
 	}()
 
-	// Run the function that writes to stdout
 	errRun := fn()
 	if errRun != nil {
-		err = errRun // Store this error to return later after cleaning up
+		err = errRun
 	}
 
-	// You must close the writer or the copy in the goroutine will never complete
 	if errClose := w.Close(); errClose != nil {
-		err = errClose // Store this error to return later after cleaning up
+		err = errClose
 	}
 
-	// Wait for capturing to finish
 	<-captureDone
-
-	// Restore original stdout
 	os.Stdout = originalStdout
-
-	// Return the captured output and any error that occurred
 	return buf.String(), err
 }
