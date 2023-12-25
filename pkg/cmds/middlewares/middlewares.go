b32ae0db2c0735d557de9a72f56afee3f4a54058
Manuel Odendahl <wesen@ruinwesen.com>
2023-12-25 20:20:28 +0100

    :tractor: Move tests that are not related from cobra to parse_test.go

--- pkg/cmds/middlewares/middlewares.go
+++ pkg/cmds/middlewares/middlewares.go
@@ -2,4 +2,6 @@ package middlewares
 
 import "github.com/go-go-golems/glazed/pkg/cmds/layers"
 
-type Handler func(layer *layers.ParameterLayer)
+type HandlerFunc func(layers *layers.ParameterLayers, parsedLayers *layers.ParsedLayers)
+
+type Middleware func(HandlerFunc) HandlerFunc
