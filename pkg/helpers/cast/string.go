fbca878aba4e8093f10513820c9257a73f9f87d3
Manuel Odendahl <wesen@ruinwesen.com>
2024-07-03 09:02:33 -0400

    :sparkles: Add helper functions to cast lists to string lists

--- pkg/helpers/cast/string.go
+++ pkg/helpers/cast/string.go
@@ -27,3 +27,35 @@ func ToString(value interface{}) (string, error) {
 
 	return "", fmt.Errorf("cannot convert %v to string", t)
 }
+
+// CastListToStringList casts a list of items to a list of strings.
+// It handles both string aliases and string declarations.
+func CastListToStringList(list interface{}) ([]string, error) {
+	val := reflect.ValueOf(list)
+
+	// Check if the value is a slice or array
+	if val.Kind() != reflect.Slice && val.Kind() != reflect.Array {
+		return nil, fmt.Errorf("the provided value is not a list")
+	}
+
+	result := make([]string, val.Len())
+	for i := 0; i < val.Len(); i++ {
+		str, err := ToString(val.Index(i).Interface())
+		if err != nil {
+			return nil, err
+		}
+		result[i] = str
+	}
+
+	return result, nil
+}
+
+// CastStringListToList casts a list of strings to a list of a specified type T.
+// The target type T must be a string alias or string declaration.
+func CastStringListToList[T ~string](list []string) ([]T, error) {
+	result := make([]T, len(list))
+	for i, str := range list {
+		result[i] = T(str)
+	}
+	return result, nil
+}
