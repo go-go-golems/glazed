8f5649e00dece6fe6f5365fa68a5e05f19c47125
Manuel Odendahl <wesen@ruinwesen.com>
2024-07-05 13:30:12 -0400

    :sparkles: Implement tail mode for json files

--- cmd/glaze/cmds/json.go
+++ cmd/glaze/cmds/json.go
@@ -1,6 +1,7 @@
 package cmds
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
@@ -14,6 +15,7 @@ import (
 	"github.com/pkg/errors"
 	"io"
 	"os"
+	"time"
 )
 
 type JsonCommand struct {
@@ -26,6 +28,7 @@ type JsonSettings struct {
 	InputIsArray bool     `glazed.parameter:"input-is-array"`
 	Sanitize     bool     `glazed.parameter:"sanitize"`
 	FromMarkdown bool     `glazed.parameter:"from-markdown"`
+	TailMode     bool     `glazed.parameter:"tail"`
 	InputFiles   []string `glazed.parameter:"input-files"`
 }
 
@@ -34,7 +37,6 @@ func NewJsonCommand() (*JsonCommand, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "could not create Glazed parameter layer")
 	}
-
 	return &JsonCommand{
 		CommandDescription: cmds.NewCommandDescription(
 			"json",
@@ -56,6 +58,12 @@ func NewJsonCommand() (*JsonCommand, error) {
 					"from-markdown",
 					parameters.ParameterTypeBool,
 					parameters.WithHelp("Input is markdown"),
+					parameters.WithDefault(false),
+				),
+				parameters.NewParameterDefinition(
+					"tail",
+					parameters.ParameterTypeBool,
+					parameters.WithHelp("Tail mode: read one JSON object per line"),
 					parameters.WithDefault(false),
 				),
 			),
@@ -86,15 +94,12 @@ func (j *JsonCommand) RunIntoGlazeProcessor(ctx context.Context, parsedLayers *l
 		}
 		var err error
 		var f io.Reader
-
 		if s.Sanitize || s.FromMarkdown {
 			b, err := os.ReadFile(arg)
 			if err != nil {
 				return errors.Wrapf(err, "Error reading file %s", arg)
 			}
-
 			s := json2.SanitizeJSONString(string(b), s.FromMarkdown)
-
 			f = bytes.NewReader([]byte(s))
 		} else {
 			f, err = os.Open(arg)
@@ -103,35 +108,86 @@ func (j *JsonCommand) RunIntoGlazeProcessor(ctx context.Context, parsedLayers *l
 			}
 		}
 
-		if s.InputIsArray {
-			data := make([]types.Row, 0)
-			err = json.NewDecoder(f).Decode(&data)
-			if err != nil {
-				return errors.Errorf("Error decoding file %s as array", arg)
-			}
-
-			i := 1
-			for _, d := range data {
-				err = gp.AddRow(ctx, d)
-				if err != nil {
-					return errors.Wrapf(err, "Error processing row %d of file %s as object", i, arg)
-				}
-				i++
-			}
+		if s.TailMode {
+			err = processTailMode(ctx, f, gp, arg)
+		} else if s.InputIsArray {
+			err = processArrayMode(ctx, f, gp, arg)
 		} else {
-			// read json file
-			data := types.NewRow()
-			err = json.NewDecoder(f).Decode(&data)
-			if err != nil {
-				return errors.Wrapf(err, "Error decoding file %s as object", arg)
-			}
+			err = processObjectMode(ctx, f, gp, arg)
+		}
 
-			err = gp.AddRow(ctx, data)
-			if err != nil {
-				return errors.Wrapf(err, "Error processing file %s as object", arg)
-			}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func processTailMode(ctx context.Context, f io.Reader, gp middlewares.Processor, arg string) error {
+	file, ok := f.(*os.File)
+	if ok {
+		_, err := file.Seek(0, io.SeekEnd)
+		if err != nil {
+			return err
 		}
 	}
 
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			return errors.Wrap(err, "Error reading line from file")
+		}
+
+		if len(line) > 0 {
+			var data types.Row
+			err := json.Unmarshal(line, &data)
+			if err != nil {
+				return errors.Wrap(err, "Error decoding line as object")
+			}
+			err = gp.AddRow(ctx, data)
+			if err != nil {
+				return errors.Wrap(err, "Error processing line as object")
+			}
+		}
+
+		if err == io.EOF {
+			// Check if we should continue or exit
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+		}
+	}
+}
+
+func processArrayMode(ctx context.Context, f io.Reader, gp middlewares.Processor, arg string) error {
+	data := make([]types.Row, 0)
+	err := json.NewDecoder(f).Decode(&data)
+	if err != nil {
+		return errors.Errorf("Error decoding file %s as array", arg)
+	}
+	for i, d := range data {
+		err = gp.AddRow(ctx, d)
+		if err != nil {
+			return errors.Wrapf(err, "Error processing row %d of file %s as object", i+1, arg)
+		}
+	}
+	return nil
+}
+
+func processObjectMode(ctx context.Context, f io.Reader, gp middlewares.Processor, arg string) error {
+	data := types.NewRow()
+	err := json.NewDecoder(f).Decode(&data)
+	if err != nil {
+		return errors.Wrapf(err, "Error decoding file %s as object", arg)
+	}
+	err = gp.AddRow(ctx, data)
+	if err != nil {
+		return errors.Wrapf(err, "Error processing file %s as object", arg)
+	}
 	return nil
 }
