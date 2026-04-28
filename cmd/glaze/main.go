391a62fdba69a0292b53c19d48b27a143ab4c5dc
Manuel Odendahl <wesen@ruinwesen.com>
2026-04-28 08:57:37 -0400

    feat: add glaze help export verb with tabular, files, and sqlite modes

--- cmd/glaze/main.go
+++ cmd/glaze/main.go
@@ -35,6 +35,12 @@ func main() {
 
 	help_cmd.SetupCobraRootCommand(helpSystem, rootCmd)
 
+	// Add the export subcommand to help.
+	helpCmd, _, err := rootCmd.Find([]string{"help"})
+	cobra.CheckErr(err)
+	err = help_cmd.AddExportCommand(helpCmd, helpSystem)
+	cobra.CheckErr(err)
+
 	// Add the serve command (embeds the React SPA via pkg/web).
 	spaHandler, err := web.NewSPAHandler()
 	cobra.CheckErr(err)
